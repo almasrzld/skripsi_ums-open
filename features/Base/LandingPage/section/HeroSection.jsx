3688c9672d692e9eb0e27158aa5409c76d8d5742
Muhammad Almas Rizaldi <almasrzld@gmail.com>
2025-05-07 19:04:53 +0700

    fix: Fix Hero Section

--- features/Base/LandingPage/section/HeroSection.jsx
+++ features/Base/LandingPage/section/HeroSection.jsx
@@ -12,15 +12,15 @@ const HeroSection = () => {
   return (
     <section className="relative max-h-[100vh] h-[100vh] bg-[#3EC1D3]">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full container">
-        <div className="flex justify-between items-center gap-8">
-          <div className="flex flex-col gap-6">
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-4 md:gap-8">
+          <div className="flex flex-col gap-2 md:gap-6">
             <h1 className="md:text-5xl sm:text-4xl text-3xl font-bold text-white">
               Kejuaraan Taekwondo
             </h1>
             <h1 className="md:text-5xl sm:text-4xl text-3xl font-bold text-white">
               Tingkat Nasional
             </h1>
-            <p className="font-light text-white text-lg">
+            <p className="font-light text-white text-md md:text-lg">
               Sistem Informasi dan Manajemen Kejuaraan Taekwondo di Universitas
               Muhammadiyah Surakarta
             </p>
@@ -40,10 +40,11 @@ const HeroSection = () => {
             </Link>
           </div>
           <Image
-            src="/images/hero.png"
+            src="/images/hero-image.png"
             alt="hero img"
             width={500}
             height={500}
+            className="md:w-[500px] md:h-[500px] w-[300px] h-[300px]"
           />
         </div>
       </div>
