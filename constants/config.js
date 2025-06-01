1a525ea8fdd9cdca074c13590d3f88226c97a290
almasrzld <almasrzld@gmail.com>
2025-06-01 20:13:02 +0700

    feat: Create app url

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,5 @@
 export const LIVE_URL = process.env.NEXT_PUBLIC_LIVE_URL;
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const MONGO_URL = process.env.MONGODB;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const MIDTRANS_CLIENT_KEY = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY;
